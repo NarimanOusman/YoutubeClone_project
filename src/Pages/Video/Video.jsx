0487ceda45c650410ef5a80b36054df22c30f0d0
NarimanOusman <narimanrobo8@gmail.com>
2025-09-03 17:02:12 +0200

    last commit

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./Video.css";
 import PlayVideo from "../../Components/playvideo/playvideo";
-import { useParams } from "react-router-dom";
 
 // Video.jsx
 const Video = ({ sidebar }) => {
-  const { videoId, categoryId } = useParams();
   return (
     <div className="played-container">
       <PlayVideo sidebar={sidebar} videoId={videoId} />
